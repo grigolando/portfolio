baecbf20a13bcee718ce6adc2a4b319bf4947252
nika grigolia <nikagrigolia2011@gmail.com>
2025-09-14 14:25:42 +0400

    This is the first commit and nothing is here

---
+++ script.js
@@ -0,0 +1,10 @@
+Questions = [
+    "Wonna learn programming?",
+    "Wonna play Game?",
+    "Wonna make business?",
+    "Wonna make exercises?"
+]
+
+const random_quetion = Questions[Math.floor(Math.random() * Questions.length)]
+
+console.log(random_quetion)
